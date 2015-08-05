667d7a21982c34b9a3176cf416327887597293c0
John Salvino <jsalvino@me.com>
2015-08-05 10:47:40 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+var flickrWidget = {};
+flickrWidget.apiUrl = 'https://api.flickr.com/services/rest/';
+flickrWidget.apiMethod = 'flickr.photos.search';
+flickrWidget.apiKey = '0716f25eaf06d2f3f4bcfb5aad2226b1';
+
+flickrWidget.getInfo = function(searchQuery) {
+	$.ajax({
+		url: flickrWidget.apiUrl,
+		format: 'json',
+		type: 'GET',
+		data: {
+			method: flickrWidget.apiMethod,
+			api_key: flickrWidget.apiKey,
+			format: 'json',
+			nojsoncallback: '1',
+			sort: 'relevance',
+			tag_mode: 'all',
+			tags: searchQuery + " ,street, b&w"
+		},
+		success: function(res) {
+			flickrWidget.displayInfo(res.photos.photo);
+		} //end success
+	}); // end ajax
+};  // end .getInfo function
+
+flickrWidget.displayInfo = function(result) {
+	$('.image-gallery').empty();
+	$.each(result, function(index, item) {
+		var $photoContainer = $('<div>');
+		$photoContainer.addClass('photo');
+		var farmID = item.farm;
+		var serverID = item.server;
+		var secretID = item.secret;
+		var ID = item.id;
+		var picUrl = 'https://farm' + farmID + '.staticflickr.com/' + serverID + '/' + ID + '_' + secretID + '.jpg';
+		var $photoImage = $('<img>');
+		$photoImage.attr('src', picUrl);
+		$photoContainer.append($photoImage);
+		$('.image-gallery').append($photoContainer);
+	}); //end Each function
+}; //end .displayInfo function
+
+flickrWidget.searchPhotos = function() {
+	$('header form').on('submit', function(evnt) {
+		$('html,body').animate({scrollTop: $('#image-gallery').offset().top},2000);
+		evnt.preventDefault();  //to stop page from refreshing page upon submit
+		$('.credits').addClass('animated fadeOut');
+		var searchQuery = $('.query').val();
+		flickrWidget.getInfo(searchQuery);
+	}); //end submit function
+}; // end .searchPhotos function
+
+flickrWidget.init = function() {
+	// flickrWidget.getInfo();
+	flickrWidget.searchPhotos();
+}; //end init function
+
+$(document).ready(function(){
+  flickrWidget.init();
+});  // end ready function
